2a79bf1d6b00874598ad17e72dbd51a40a20679d
sanAkdam <san.akdam@gmail.com>
2015-12-02 23:36:51 +0700

    Added database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "sanAkdam", email: "san.akdam@gmail.com", password: "san190896", password_confirmation: "san190896")
+User.create(username: "sanAkdam1", email: "san.akdam1@gmail.com", password: "san190896", password_confirmation: "san190896")
+User.create(username: "sanAkdam2", email: "san.akdam2@gmail.com", password: "san190896", password_confirmation: "san190896")
+User.create(username: "sanAkdam3", email: "san.akdam3@gmail.com", password: "san190896", password_confirmation: "san190896")
+User.create(username: "sanAkdam4", email: "san.akdam4@gmail.com", password: "san190896", password_confirmation: "san190896")
+
+puts "User Create"
